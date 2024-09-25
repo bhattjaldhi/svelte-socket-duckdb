8edf73d167ac1b148523c437bf95cf8d333f2fce
bhattjaldhi <bhattjaldhi27@gmail.com>
2024-09-25 18:27:19 -0400

    (feat) add 3 envs for dev, test and prod

--- flask-app/config.py
+++ flask-app/config.py
@@ -1,12 +1,18 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+# Load the appropriate .env file based on the FLASK_ENV
+flask_env = os.environ.get('FLASK_ENV', 'development')
+if flask_env == 'testing':
+    load_dotenv('.env.test')
+else:
+    load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
-    DB_FILE_PATH = os.path.join(os.path.dirname(__file__), 'data', 'database.db')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    DB_FILE_PATH = os.path.join(os.path.dirname(__file__), 'data', 'database.db')    
+    FLASK_ENV = os.environ.get('FLASK_ENV')
 
 class TestingConfig(Config):
     TESTING = True
-    DB_FILE_PATH = os.path.join(os.path.dirname(__file__), 'tests', 'testdatabase.db')  # Use in-memory database for testing
+    DB_FILE_PATH = os.path.join(os.path.dirname(__file__), 'tests', 'test_database.db')
--- flask-app/entrypoint.sh
+++ flask-app/entrypoint.sh
@@ -4,14 +4,20 @@ set -e
 # Run database seeding script
 python scripts/seed_db.py
 
-# Check the command
+# Check the command and environment
 if [ "$1" = "run" ]; then
     if [ "$FLASK_ENV" = "development" ]; then
         echo "Running in development mode..."
         python run.py
-    else
+    elif [ "$FLASK_ENV" = "production" ]; then
         echo "Running in production mode..."
         gunicorn --worker-class eventlet -w 1 -b 0.0.0.0:5000 run:app
+    elif [ "$FLASK_ENV" = "testing" ]; then
+        echo "Running tests..."
+        pytest
+    else
+        echo "Unknown FLASK_ENV: $FLASK_ENV"
+        exit 1
     fi
 elif [ "$1" = "bash" ]; then
     exec /bin/bash
